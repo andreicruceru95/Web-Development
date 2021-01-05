49c27cf1d8a728a6f16626e888c472723d202d45
andreicruceru95 <56835442+andreicruceru95@users.noreply.github.com>
2021-01-05 14:40:49 +0000

    Create what-is.js

---
+++ static/what-is/js/what-is.js
@@ -0,0 +1,85 @@
+function what(){
+    div = document.getElementById('what-ds');
+    div.style.display = "block";
+    div2 = document.getElementById('why-ds');
+    div2.style.display = "none";
+    div3 = document.getElementById('foundations-ds');
+    div3.style.display = "none";
+    div4 = document.getElementById('relations-ds');
+    div4.style.display = "none";
+    div5 = document.getElementById('early-ds');
+    div5.style.display = "none";
+    div6 = document.getElementById('modern-ds');
+    div6.style.display = "none";
+}
+function why(){
+    div = document.getElementById('why-ds');
+    div.style.display = "block";
+    div2 = document.getElementById('what-ds');
+    div2.style.display = "none";
+    div3 = document.getElementById('foundations-ds');
+    div3.style.display = "none";
+    div4 = document.getElementById('relations-ds');
+    div4.style.display = "none";
+    div5 = document.getElementById('early-ds');
+    div5.style.display = "none";
+    div6 = document.getElementById('modern-ds');
+    div6.style.display = "none";    
+}
+function foundations(){
+    div = document.getElementById('foundations-ds');
+    div.style.display = "block";
+    div2 = document.getElementById('why-ds');
+    div2.style.display = "none";
+    div3 = document.getElementById('what-ds');
+    div3.style.display = "none";
+    div4 = document.getElementById('relations-ds');
+    div4.style.display = "none";
+    div5 = document.getElementById('early-ds');
+    div5.style.display = "none";
+    div6 = document.getElementById('modern-ds');
+    div6.style.display = "none";
+   
+}
+function relations(){
+    div = document.getElementById('relations-ds');
+    div.style.display = "block";
+    div2 = document.getElementById('why-ds');
+    div2.style.display = "none";
+    div3 = document.getElementById('foundations-ds');
+    div3.style.display = "none";
+    div4 = document.getElementById('what-ds');
+    div4.style.display = "none";
+    div5 = document.getElementById('early-ds');
+    div5.style.display = "none";
+    div6 = document.getElementById('modern-ds');
+    div6.style.display = "none";
+}
+function early(){
+    div = document.getElementById('early-ds');
+    div.style.display = "block";
+    div2 = document.getElementById('why-ds');
+    div2.style.display = "none";
+    div3 = document.getElementById('foundations-ds');
+    div3.style.display = "none";
+    div4 = document.getElementById('what-ds');
+    div4.style.display = "none";
+    div5 = document.getElementById('relations-ds');
+    div5.style.display = "none";
+    div6 = document.getElementById('modern-ds');
+    div6.style.display = "none";
+}
+function modern(){
+    div = document.getElementById('modern-ds');
+    div.style.display = "block";
+    div2 = document.getElementById('why-ds');
+    div2.style.display = "none";
+    div3 = document.getElementById('foundations-ds');
+    div3.style.display = "none";
+    div4 = document.getElementById('what-ds');
+    div4.style.display = "none";
+    div5 = document.getElementById('early-ds');
+    div5.style.display = "none";
+    div6 = document.getElementById('relations-ds');
+    div6.style.display = "none";
+}
