f9faf2f6663706cfe19c41c257065f3d289b2d47
andreicruceru95 <56835442+andreicruceru95@users.noreply.github.com>
2021-01-05 14:40:02 +0000

    Create about.js

---
+++ static/about/js/about.js
@@ -0,0 +1,41 @@
+function displayMl1(){
+    div = document.getElementById('ml1');
+    div.style.display = "block";
+    div2 = document.getElementById('ml2');
+    div2.style.display = "none";
+    div3 = document.getElementById('ml3');
+    div3.style.display = "none";
+    div4 = document.getElementById('ml4');
+    div4.style.display = "none";
+}
+function displayMl2(){
+    div = document.getElementById('ml2');
+    div.style.display = "block";
+    div2 = document.getElementById('ml3');
+    div2.style.display = "none";
+    div3 = document.getElementById('ml4');
+    div3.style.display = "none";
+    div4 = document.getElementById('ml1');
+    div4.style.display = "none";
+}
+function displayMl3(){
+    div = document.getElementById('ml3');
+    div.style.display = "block";
+    div2 = document.getElementById('ml4');
+    div2.style.display = "none";
+    div3 = document.getElementById('ml1');
+    div3.style.display = "none";
+    div4 = document.getElementById('ml2');
+    div4.style.display = "none";
+   
+}
+function displayMl4(){
+    div = document.getElementById('ml4');
+    div.style.display = "block";
+    div2 = document.getElementById('ml3');
+    div2.style.display = "none";
+    div3 = document.getElementById('ml2');
+    div3.style.display = "none";
+    div4 = document.getElementById('ml1');
+    div4.style.display = "none";
+}
