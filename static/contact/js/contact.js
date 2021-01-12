501acb2c6a447366cc1f33e4f8d0677498f00762
andreicruceru95 <56835442+andreicruceru95@users.noreply.github.com>
2021-01-12 15:26:47 +0000

    Update contact.js

--- static/contact/js/contact.js
+++ static/contact/js/contact.js
@@ -4,27 +4,88 @@ window.addEventListener("DOMContentLoaded", function() {
     
     var form = document.getElementById("my-form");
     var status = document.getElementById("status");
+    const fName = document.getElementById("firstName");
+    const lName = document.getElementById("lastName");
+    const email = document.getElementById("email");
+    const message = document.getElementById("message");
+    var text = document.getElementById('VFname')
+    var text1 = document.getElementById('VLname')
+    var text2 = document.getElementById('Vemail')
+    var text3 = document.getElementById('Vmessage')
 
     // Success and Error functions for after the form is submitted
     
     function success() {
       form.reset();
-      status.classList.add('success');
-      status.innerHTML = "Thanks!";
+      status.style.opacity = "1";
+      status.innerHTML = "Success.";
+      status.style.color = "green";
+      setInterval(fade, 5000);
     }
-
     function error() {
-      status.classList.add('error');
+      status.style.opacity = "1";
       status.innerHTML = "Oops! There was a problem.";
+      status.style.color = "red";
+      setInterval(fade, 5000);
+    } 
+    function fade() {
+      status.style.opacity = "0";
     }
-
+    function changeText(){
+      text.style.opacity = "0";
+    }    
+    function changeText1(){
+      text1.style.opacity = "0";
+    } 
+    function changeText2(){
+      text2.style.opacity = "0";
+    } 
+    function changeText3(){
+      text3.style.opacity = "0";
+    } 
+      
     // handle the form submission event
-
-    form.addEventListener("submit", function(ev) {
+    var submit=document.querySelector("#submit");
+    submit.addEventListener("click", function(ev) {      
       ev.preventDefault();
-      var data = new FormData(form);
-      ajax(form.method, form.action, data, success, error);
-    });
+      if(fName.value ==='' || fName.value == null || lName.value ==='' || lName.value == null || email.value ==='' || email.value == null || message.value ==='' || message.value == null)
+     {
+      if(fName.value ==='' || fName.value == null) {
+        text.style.opacity = "1";
+        text.style.display = "block";
+        setInterval(changeText, 5000);           
+      }
+      else{
+        text.style.display = "none";
+      }
+      if(lName.value ==='' || lName.value == null) {  
+        text1.style.opacity = "1";      
+        text1.style.display = "block"; 
+        setInterval(changeText1, 5000); 
+      }
+      else{
+        text1.style.display = "none"; 
+      }
+      if(email.value ==='' || email.value == null) {        
+       
+        text2.style.opacity = "1";       
+        text2.style.display = "block"; 
+        setInterval(changeText2, 5000);  
+      }
+      else{
+        text2.style.display = "none"; 
+      }
+      if(message.value ==='' || message.value == null) {   
+        text3.style.opacity = "1";     
+        text3.style.display = "block";  
+        setInterval(changeText3, 5000); 
+      }
+      else{
+        text3.style.display = "none"; 
+      }}
+      else{var data = new FormData(form);      
+        ajax(form.method, form.action, data, success, error);}
+    });  
   });
   
   // helper function for sending an AJAX request
@@ -37,9 +98,18 @@ window.addEventListener("DOMContentLoaded", function() {
       if (xhr.readyState !== XMLHttpRequest.DONE) return;
       if (xhr.status === 200) {
         success(xhr.response, xhr.responseType);
+        text.style.display = "none";
+        text1.style.display = "none";
+        text2.style.display = "none";
+        text3.style.display = "none";
       } else {
         error(xhr.status, xhr.response, xhr.responseType);
+        text.style.display = "none";
+        text1.style.display = "none";
+        text2.style.display = "none";
+        text3.style.display = "none";
       }
     };
-    xhr.send(data);
+    xhr.send(data);      
   }
+  
